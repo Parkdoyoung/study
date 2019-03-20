feb941154183b6d0b17adcbb5d4fcb714e6e824b
park <swko94@naver.com>
2019-03-20 11:12:07 +0900

    apple

--- hello.py
+++ hello.py
@@ -1,10 +1,6 @@
 // For comman line git tutorial
 // Local repository modification
-<<<<<<< HEAD
 COFFEE
-=======
-SNOOPY
->>>>>>> origin/master
 print("Hello world")
 print("tell your world")
 print("tell his world")
